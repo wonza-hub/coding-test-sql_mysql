126d2d9994653efe66fc51ea20ab5a50155fa979
wonza-hub <wonza8585@gmail.com>
2025-03-07 23:06:11 +0900

    [리트코드] Game Play Analysis 4

---
+++ WHERE...IN/Game Play Analysis 4.sql
@@ -0,0 +1,8 @@
+SELECT ROUND((COUNT(DISTINCT player_id))/(SELECT COUNT(DISTINCT player_id) FROM Activity),2) AS fraction
+FROM Activity
+WHERE (player_id,event_date)
+IN (
+    SELECT player_id,DATE_ADD(MIN(event_date),INTERVAL 1 DAY) -- DATE_ADD(SUB)
+    FROM Activity
+    GROUP BY player_id
+)
