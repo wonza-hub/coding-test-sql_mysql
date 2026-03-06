69fa3455aae9bf2d22eab08bea6fc9706c70c0d7
wonza-hub <wonza8585@gmail.com>
2026-03-06 16:35:56 +0900

    (복습) [리트코드] Consecutive Numbers

--- 윈도우 함수/LAG,LEAD/Consecutive Numbers.sql
+++ 윈도우 함수/LAG,LEAD/Consecutive Numbers.sql
@@ -6,3 +6,18 @@ SELECT id,num,LAG(num,1) OVER (ORDER BY id) AS numm,LAG(num,2) OVER (ORDER BY id
 FROM Logs
 ) A
 WHERE A.num=A.numm AND A.numm=A.nummm
+
+-- 복습 풀이
+-- DINSTINCT: 연속 4번 이상 나온 경우 중복 제거해야 함에 유의
+WITH DISTINCT CTE AS
+(
+    SELECT 
+    NUM,
+    LAG(NUM,1) OVER(ORDER BY ID) NUMM,
+    LAG(NUM,2) OVER(ORDER BY ID) NUMMM
+    FROM LOGS
+)
+
+SELECT NUM ConsecutiveNums
+FROM CTE
+WHERE NUM=NUMM AND NUMM=NUMMM
