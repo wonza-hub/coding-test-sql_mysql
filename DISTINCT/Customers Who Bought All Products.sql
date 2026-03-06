8c4cf6134f0d978d660ca5af1a19ba3757f07541
wonza-hub <wonza8585@gmail.com>
2026-03-06 16:46:15 +0900

    (복습) [리트코드] Customers Who Bought All Products

--- DISTINCT/Customers Who Bought All Products.sql
+++ DISTINCT/Customers Who Bought All Products.sql
@@ -8,3 +8,14 @@ SELECT customer_id
 FROM Customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key)=(SELECT total FROM cte)
+
+-- 복습 풀이
+SELECT CUSTOMER_ID
+FROM CUSTOMER
+GROUP BY CUSTOMER_ID
+-- DISTINCT: 동일한 제품을 여러 번 구매한 경우 고려
+HAVING COUNT(DISTINCT PRODUCT_KEY)=
+(
+    SELECT COUNT(*)
+    FROM PRODUCT
+)
